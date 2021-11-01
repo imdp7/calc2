d0ecd1cf065c039d7cffb0606f8d73119fdf5dfd
codyazari <azaricody@gmail.com>
2021-11-01 16:58:51 -0400

    Optimized calculator.py, added a division function

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,5 +1,5 @@
 """Testing the Calculator"""
-from calculator.main import Calculator
+from calculator.calculator import Calculator
 
 def test_calculator_result():
     """testing calculator result is 0"""
@@ -7,13 +7,7 @@ def test_calculator_result():
     assert calc.result == 0
 
 def test_calculator_add():
-    """Testing the Add function of the calculator"""
-    #Arrange by instantiating the calc class
-    calc = Calculator()
-    #Act by calling the method to be tested
-    calc.add_number(4)
-    #Assert that the results are correct
-    assert calc.result == 4
+    assert Calculator.add_number(1,3) == 3
 
 def test_calculator_get_result():
     """Testing the Get result method of the calculator"""
@@ -29,4 +23,9 @@ def test_calculator_multiply():
     """ tests multiplication of two numbers"""
     calc = Calculator()
     result  = calc.multiply_numbers(1,2)
+    assert result == 2
+def test_calculator_divide():
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result  = calc.divide_numbers(4,2)
     assert result == 2

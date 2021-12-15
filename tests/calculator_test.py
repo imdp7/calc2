4d5a6583932e493bc1f3dc8edbeee458bc67c806
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 06:54:52 -0500

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -1,52 +1,37 @@
-"""Testing the Calculator"""
+from calculator.main import Calculator
 import pytest
-from calculator.calculator import Calculator
 
+def test_calculator_result():
+    """testing calculator result is 0"""
+    calc = Calculator()
+    assert calc.result == 0
+    
 def test_calculator_add():
     """Testing the Add function of the calculator"""
-    # Arrange
-    value_a = 1
-    value_b = 2
-    # Act
-    result = Calculator.add_number(value_a,value_b)
-    # Assert
-    assert result == 3
+    #Arrange by instantiating the calc class
+    calc = Calculator()
+    #Act by calling the method to be tested
+    calc.add_number(1)
+    #Assert that the results are correct
+    assert calc.result == 1
 
+def test_calculator_get_result():
+    """Testing the Get result method of the calculator"""
+    calc = Calculator()
+    assert calc.get_result() == 0
+    
 def test_calculator_subtract():
     """Testing the subtract method of the calculator"""
-    # Arrange
-    value_a = 1
-    value_b = 2
-    # Act
-    result = Calculator.subtract_number(value_a, value_b)
-    # Assert
-    assert result == -1
+    calc = Calculator()
+    calc.subtract_number(1)
+    assert calc.get_result() == -1
 
 def test_calculator_multiply():
-    """ Testing multiplication of two numbers"""
-    # Arrange
-    value_a = 1
-    value_b = 2
-    # Act
-    result = Calculator.multiply_numbers(value_a, value_b)
-    # Assert
-    assert result == 2
-
+    """ tests multiplication of two numbers"""
+    calc = Calculator()
+    result  = calc.multiply_numbers(1,2)
+    
 def test_calculator_division():
     """ Testing division of two numbers"""
-    # Arrange
-    value_a = 1
-    value_b = 1
-    # Act
-    result = Calculator.divide_numbers(value_a, value_b)
-    # Assert
-    assert result == 1
-
-def test_calculator_division_exception():
-    """ Testing division exception for division by zero"""
-    # Arrange
-    value_a = 1
-    value_b = 0
-    # Act
-    with pytest.raises(ZeroDivisionError):
-        Calculator.divide_numbers(value_a,value_b)
+    calc = Calculator()
+    result = calc.divide_numbers(1,1)

3cd3d1b4882318fdb7b9074f903cc6debed6bafa
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 09:16:56 -0500

    Update calculator_test.py

--- tests/calculator_test.py
+++ tests/calculator_test.py
@@ -16,7 +16,7 @@ def test_calculator_add_static(clear_history_fixture):
 def test_calculator_subtract_static(clear_history_fixture):
     """Testing the subtract method of the calc"""
     # pylint: disable=unused-argument,redefined-outer-name
-    assert Calculator.subtract_numbers(1.0,2.0) == -1.0
+    assert Calculator.subtract_numbers(1.0,2.0) == -3.0
 
 def test_calculator_multiply_static(clear_history_fixture):
     """Testing the multiplication method of the calc"""

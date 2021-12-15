f4001f8590ea5363b02eaa3b6130778e2a2841ef
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 09:03:08 -0500

    Update multiplication_test.py

--- tests/multiplication_test.py
+++ tests/multiplication_test.py
@@ -1,12 +1,11 @@
-""" Multiplication Tests """
-from calculator.calculations.multiplication import Multiplication
+"""Testing Multiplication"""
+from calc.calculations.multiplication import Multiplication
 
-
-def multiplication_test_calculation():
-    """ testing static method for addition """
-    # Arrange
-    numbers = (10.0, 10.0)
-    multiplication = Multiplication(numbers)
-    # Act
-    # Assert
-    assert multiplication.get_result() == 100.0
+def test_calculation_multiplication():
+    """testing that our calculator has a static method for multiplication"""
+    #Arrange
+    mynumbers = (1.0,2.0,3.0)
+    multiplication = Multiplication(mynumbers)
+    #Act
+    #Assert
+    assert multiplication.get_result() == 6.0

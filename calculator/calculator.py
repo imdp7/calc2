f17dc9967fbd7ec76d4620e28c23993fb11fc933
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 07:01:34 -0500

    Update calculator.py

--- calculator/calculator.py
+++ calculator/calculator.py
@@ -1,23 +1,25 @@
+""" This is the increment function"""
 class Calculator:
     """ This is the Calculator class"""
 
-    result = 0
-    def get_result(self):
-        """ Get Result of Calculation"""
-        return self.result
-    def add_number(self, value_a):
-         """ adds number to result"""
-        self.result = self.result + value_a
-        return self.result
-    def subtract_number(self, value_a):
-         """ subtract number from result"""
-        self.result = self.result - value_a
-        return self.result
-    def multiply_numbers(self, value_a, value_b):
-         """ multiply two numbers and store the result"""
-        self.result = value_a * value_b
-        return self.result
-    def divide_numbers(self, value_a, value_b):
-        self.result = value_a / value_b
-        return self.result
+    history = []
+
+    @staticmethod
+    def add_number(value_a, value_b):
+        """ adds number to result"""
+        return value_a + value_b
+
+    @staticmethod
+    def subtract_number(value_a, value_b):
+        """ subtract number from result"""
+        return value_a - value_b
+
+    @staticmethod
+    def multiply_numbers( value_a, value_b):
+        """ multiply two numbers and store the result"""
+        return value_a * value_b
+
+    @staticmethod
+    def divide_numbers( value_a, value_b):
+        """ divide two numbers and store the result"""
         return value_a / value_b

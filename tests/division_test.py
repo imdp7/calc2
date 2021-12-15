2adfbc79da31919791f96c9988c03651019e1770
Darshan Patel <44410769+imdp7@users.noreply.github.com>
2021-12-15 08:51:59 -0500

    Update division_test.py

--- tests/division_test.py
+++ tests/division_test.py
@@ -1,12 +1,24 @@
-""" Division Tests """
-from calculator.calculations.division import Division
+"""Testing Division"""
+import pytest
+from calc.calculations.division import Division
 
+def test_calculation_division():
+    """testing that our calculator has a static method for multiplication"""
+    #Arrange
+    mynumbers = (1.0,2.0,4.0)
+    division = Division(mynumbers)
+    #Act
+    #Assert
+    assert division.get_result() == 0.125
 
-def division_test_calculation():
-    """ Testing static method for division """
+def test_calculator_division_exception():
+    """ Testing division exception for division by zero"""
     # Arrange
-    numbers = (15.0, 3.0)
-    division = Division(numbers)
+    mynumbers = (1.0, 0.0, 1.0)
+    division = Division(mynumbers)
     # Act
     # Assert
-    assert division.get_result() == 5.0
+    with pytest.raises(ZeroDivisionError):
+        #import pdb;pdb.set_trace()
+        result = division.get_result()
+        assert result is True
